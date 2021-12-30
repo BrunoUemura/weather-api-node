80648459b8cdd9ded5e60bb5acf481fdbf718b38
BrunoUemura <bruno.h.uemura@outlook.com>
2021-12-30 01:03:11 -0300

    Add env to deployment.yml

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     testEnvironment: 'node',
     collectCoverage: true,
     collectCoverageFrom: ["./src/**"],
+    coveragePathIgnorePatterns: ["src/server.ts"],
     coverageDirectory: 'test/coverage',
     setupFiles: ["./test/setup-tests.ts"]
 };
